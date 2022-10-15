ea7bd72be71efe0ee3b113596b25b6fb7ab213a9
Kyle West <kylewestrrr@gmail.com>
2022-10-15 16:35:53 -0600

    [bullets] add bullet mechanics to the game

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { addListener } = require('./keyboard');
 const { paint, bounds, apply } = require('./print');
+const { div, physics, keepInBounds } = require('./math');
+const { addListener } = require('./keyboard');
+
+const { fire, bullets, removeDeadBullets } = require('./bullets');
 
 const board = `╔${"═".repeat(bounds.x-2)}╗\n${`║${" ".repeat(bounds.x-2)}║\n`.repeat(bounds.y-3)}╚${"═".repeat(bounds.x-2)}╝`
 
 const player = {
-  x: 10,
-  y: 4,
+  x: div(bounds.x, 2),
+  y: div(bounds.y, 2),
   draw: `■`,
   facing: 'right'
 }
@@ -13,26 +16,37 @@ const player = {
 addListener((key) => {
   switch(key) {
     case 'ArrowUp': {
-      player.y = player.y - 1 
+      player.y = player.y - 1
+      player.facing = 'up'
       break;
     }
     case 'ArrowDown': {
       player.y = player.y + 1 
+      player.facing = 'down'
       break;
     }
     case 'ArrowLeft': {
       player.x = player.x - 1 
+      player.facing = 'left'
       break;
     }
     case 'ArrowRight': {
       player.x = player.x + 1 
+      player.facing = 'right'
+      break;
+    }
+    case ' ': {
+      fire(player)
       break;
     }
   }
+  keepInBounds(player)
 })
 
 function gameLoop () {
-  paint(apply(board, player))
+  physics(...bullets)
+  removeDeadBullets()
+  paint(apply(board, player, ...bullets))
 }
 
 gameLoop()
