d187f37a6a4ff0788dcfa4b6c2ee5c1d3699a309
Kyle West <kyle-west@users.noreply.github.com>
2022-10-15 18:21:37 -0600

    [zombies] add zombies into game, death, simple menu, etc (#2)
    This PR adds several game mechanics:
    
    - Welcome and Game over menus
    - Zombies that try to follow the player
    - Bullets hit the zombies and kill them

--- print.js
+++ print.js
@@ -11,6 +11,7 @@ function clear () {
 }
 
 function stamp(str, index, replacement) {
+  if (!str) return
   return str.substring(0, index) + replacement + str.substring(index + replacement.length);
 }
 
